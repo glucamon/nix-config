ba96cc5304acd92d80d5b0dc1c2f88c49e966d6b
telometto <65364211+telometto@users.noreply.github.com>
2025-05-01 10:00:10 +0200

    🪄 feature(gui): Moved the configuration to separate files and instead use this file for easy enable/disable.

--- hosts/desktop/system/gui.nix
+++ hosts/desktop/system/gui.nix
@@ -1,17 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  services = {
-    xserver = {
-      enable = true; # Enable the X11 windowing system
-
-      # Enable the GNOME Desktop Environment
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-
-      # libinput = {
-      #   enable = false; # Enable touchpad support (enabled default in most desktopManager)
-      # };
-    };
-  };
+  imports = [
+    ./desktop-environments/gnome/gnome.nix
+    # ./desktop-environments/kde/kde.nix
+  ]
 }
