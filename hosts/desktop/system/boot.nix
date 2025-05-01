5723d3d465b49b89631ba557c2a6feb039b9cffe
telometto <65364211+telometto@users.noreply.github.com>
2025-05-01 09:58:59 +0200

    🪄 feature(boot): Added and enabled `plymouth` and added a limit to configurations.

--- hosts/desktop/system/boot.nix
+++ hosts/desktop/system/boot.nix
@@ -10,11 +10,43 @@
     loader = {
       systemd-boot = {
         enable = true;
+
+        configurationLimit = 5;
       };
 
       efi = {
         canTouchEfiVariables = true;
       };
     };
+
+    plymouth = {
+      enable = true;
+
+      theme = "spinner"; # Change to "rings" and uncomment the themePackages block to use the "rings" theme
+      # themePackages = with pkgs; [
+      #   # By default we would install all themes
+      #   (adi1090x-plymouth-themes.override {
+      #     selected_themes = [ "rings" ];
+      #   })
+      # ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
   };
 }
